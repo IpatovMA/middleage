3d19f2a096acf3bfe8d21b695a2f15bbb1d513c3
Ipatov Mikhail <mipatov0pls@ya.ru>
2019-07-12 00:17:54 +0300

    porject: двигается через addForce; уровень стакается

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int power = 70;
+  //  public float speed = 10.0f;
+    public int jumpPower = 1000;
+    public bool isFalling = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+     GetComponent<Rigidbody2D>().AddForce(Vector2.right*power);
+    //  GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0.0f);
+
+        if(Input.GetKey(KeyCode.Space)&&(!isFalling)){
+          GetComponent<Rigidbody2D>().AddForce(Vector2.up*jumpPower);
+
+          isFalling = true;
+        }
+
+    //    if
+
+    }
+
+    void OnCollisionStay2D(Collision2D coll){
+      if(coll.gameObject.tag == "Ground"){
+        isFalling=false;
+      }
+    }
+
+
+}
---
+++ Assets/Scripts/levelBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelBuilder : MonoBehaviour
+{
+    public GameObject[] Blocks;
+  //public GameObject PatternBlock;
+    public Transform BlockSpawnerPos;
+  public int BlockCount =0;
+    public float NewPos = 5.0f;
+    public int LevelLenght = 1;
+    public int blockVariants = 5;
+
+    private float waitTime = 2.0f;
+    private GameObject block;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelCreation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void levelCreation(){
+      for (BlockCount=1; BlockCount< LevelLenght; BlockCount++){
+        Block();
+      }
+    }
+
+
+    public void Block()
+    {
+      block = Instantiate (Blocks[Random.Range(0,blockVariants)], BlockSpawnerPos.position,Quaternion.identity) as GameObject;
+      Vector3 temp = BlockSpawnerPos.position;
+      temp.x += NewPos;
+      temp.y = 0;
+      temp.z  = 0;
+      BlockSpawnerPos.position = temp;
+    }
+
+}
