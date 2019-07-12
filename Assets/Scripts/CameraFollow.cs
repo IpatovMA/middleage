c9121b8aeec512f1482f326a44c2a5eac7c749ba
Ipatov Mikhail <mipatov0pls@ya.ru>
2019-07-12 15:01:44 +0300

    камера фиксирована по y; персонаж движется по velocity

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public int smoothTime = 5;
+    public float offset = 0f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+         if (target != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x + offset, transform.position.y, transform.position.z), Time.deltaTime * smoothTime);
+        }       
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,26 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+   public float speed = 10.0f;
+   public float jampSpeed = 7f;
     public int power = 70;
-  //  public float speed = 10.0f;
     public int jumpPower = 1000;
     public bool isFalling = false;
+    public float gravFactor = 0.2f;
+
+  //  private Vector2 defaultVelocity = new Vector2(10f, 0.0f);
+    private Vector2  VelocityDir = new Vector2 (1f,0f);
+
 
     // Update is called once per frame
     void Update()
     {
-     GetComponent<Rigidbody2D>().AddForce(Vector2.right*power);
-    //  GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0.0f);
-
-        if(Input.GetKey(KeyCode.Space)&&(!isFalling)){
-          GetComponent<Rigidbody2D>().AddForce(Vector2.up*jumpPower);
-
-          isFalling = true;
-        }
-
-    //    if
-
     }
+
+        void FixedUpdate()
+        {
+      /*  if (!isFalling){
+        GetComponent<Rigidbody2D>().AddForce(Vector2.right*power);
+          } else {
+          
+          }*/ 
+        //GetComponent<Rigidbody2D>().AddForce(Vector2.right*power);
+        //скорость по направлению velocitydir
+         GetComponent<Rigidbody2D>().velocity = new Vector2(speed*VelocityDir.x, jampSpeed*VelocityDir.y);
+
+
+            if(Input.GetKey(KeyCode.Space)&&(!isFalling)){
+             //GetComponent<Rigidbody2D>().AddForce(Vector2.up*jumpPower);
+              VelocityDir += new Vector2(0f,1f);
+              //VelocityDir = VelocityDir;
+              isFalling = true;
+        }
+          if(isFalling){
+              VelocityDir += new Vector2(0f,-gravFactor);
+          }else {
+            VelocityDir = new Vector2(VelocityDir.x,0f);
+          }
+        }
 
     void OnCollisionStay2D(Collision2D coll){
       if(coll.gameObject.tag == "Ground"){
@@ -31,5 +51,8 @@ public class Player : MonoBehaviour
       }
     }
 
+    void GetVelocityDir(){
+     // VelocityDir =
+    }
 
 }
